15ee7b9957a4c541b5c8bf41f3c81db6a6818465
Mohsen Farahanchi <mohsenfarahanchi@outlook.com>
2018-12-22 00:22:10 +0400

    skeleton for unit test

--- TestUnit.py
+++ TestUnit.py
@@ -1,35 +1,42 @@
 import unittest
 from booking import *
 
-class TestBookHotel(unittest.TestCase):
-    room1 = Room(12,3,250000)
-    hotel = Hotel("aras baran", "tabriz, el goli", (10,10), "04134773050")
-    user1 = User("vali", "04144422225")
-    book = Book(200, 202, room1, user1)
 
-    def test_defineHotel(self):
+class TestBookHotel(unittest.TestCase):
+    # room1 = Room(12,3,250000)
+    # hotel = Hotel("aras baran", "tabriz, el goli", (10,10), "04134773050")
+    # user1 = User("vali", "04144422225", 20)
+    # book = Book(200, 202, room1)
+
+    def test_new_hotel(self):
         # self.room1 = Room(12,3,250000)
         # self.hotel = Hotel("aras baran", "tabriz, el goli", (10,10), "04134773050")
-        self.bookee = Bookee("hotel")
+        # self.bookee = Bookee("hotel")
+        # self.hotel.addRoom(self.room1)
+        # self.bookee.addBookee(self.hotel)
+        # print(1)
+        # self.assertEqual(self.bookee.inventory[0].rooms[0], self.room1)
+        pass
 
-        self.hotel.addRoom(self.room1)
-        self.bookee.addBookee(self.hotel)
-        print(1)
-        self.assertEqual(self.bookee.inventory[0].rooms[0], self.room1)
+    def test_book_room(self):
+        # user = User("vali","04144422225", 20)
+        # book = Book(200,202,self.room1)
+        # print(2)
+        # print (book.calculateCost())
+        pass
 
-    def test_bookRoom(self):
+    def test_new_user(self):
+        pass
 
-        user = User("vali","04144422225")
-        book = Book(200,202,self.room1, user)
-        print(2)
-        print (book.calculateCost())
-
+    def test_new_book_user(self):
+        pass
 
     def test_mapUserBook(self):
-        self.map = BookInventory()
-        self.map.addMapping(self.user1, self.book)
-        print(self.map.findByBook(self.book))
-        print(self.map.findByUser(self.user1))
+        # self.map = BookInventory()
+        # self.map.addMapping(self.user1, self.book)
+        # print(self.map.findByBook(self.book))
+        # print(self.map.findByUser(self.user1))
+        pass
 
     def test_book(self):
         pass
